816e6ae2603e4686acd40e812c292f583ea972d6
Dawin <dawin002@naver.com>
2025-01-21 18:26:24 +0900

    chore(backend): node.js 개발 환경 세팅

---
+++ backend/.eslintrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+  env: {
+    browser: true,
+    node: true,
+  },
+  extends: [
+    'eslint:recommended', //
+    'plugin:prettier/recommended',
+  ],
+  rules: {
+    'prettier/prettier': ['error'],
+    'no-unused-vars': 'warn',
+    'no-console': 'off',
+  },
+};
