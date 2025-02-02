7f989ca914bac8e96e9c3f6fdfb3b23148a850f9
Dawin <dawin002@naver.com>
2025-02-02 19:13:01 +0900

    refactor(backend): crud api 보일러 플레이트 수정

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,22 +1,41 @@
 import express from 'express';
+import swaggerUi from 'swagger-ui-express';
+import swaggerJsdoc from 'swagger-jsdoc';
 
 const app = express();
 
 app.use(express.json());
 
 app.get('/boards', function (req, res) {
-    // 요청한 데이터 확인
-
     // DB에서 데이터 꺼내오기
-    const result = [{}, {}, {}];
+    const result = [
+        {
+            number: 1,
+            writer: '다윈',
+            title: '제목1',
+            contents: '1번 글의 내용입니다.',
+        },
+        {
+            number: 2,
+            writer: '철수',
+            title: '제목2',
+            contents: '2번 글의 내용입니다.',
+        },
+        {
+            number: 3,
+            writer: '수지',
+            title: '제목3',
+            contents: '3번 글의 내용입니다.',
+        },
+    ];
 
     // 요청한 데이터 응답하기
     res.send(result);
 });
 
 app.post('/boards', function (req, res) {
-    // 데이터 받기
-
+    // 요청 확인하기
+    console.log(req.body);
     // DB에 저장
 
     // DB 저장 결과 응답하기
@@ -24,7 +43,7 @@ app.post('/boards', function (req, res) {
 });
 
 app.put('/boards', function (req, res) {
-    // 데이터 받기
+    // 요청 확인하기
 
     // DB에 수정
 
