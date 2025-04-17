e0b0a8efc5e796b6eed62c15a2c9e957db4193b9
Dawin <dawin002@naver.com>
2025-04-17 22:12:20 +0900

    feat(basic-board): 프로젝트 전체 환경변수 설정

--- backend/src/config/db.ts
+++ backend/src/config/db.ts
@@ -3,7 +3,10 @@ import mongoose from 'mongoose';
 export const connectDB = async () => {
   try {
     mongoose.set('debug', true);
-    await mongoose.connect('mongodb://my-database:27017/basicBoard');
+    if (!process.env.URL) {
+      throw new Error('환경변수 URL이 설정되어 있지 않습니다.');
+    }
+    await mongoose.connect(process.env.URL);
     console.log('DB 접속 성공');
   } catch (error) {
     console.error('DB 접속 실패', error);
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,6 +1,7 @@
+import 'dotenv/config';
 import app from './app';
 
-const PORT = 3000;
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
   console.log(`서버가 ${PORT}번 포트에서 실행 중입니다.`);
 });
